c3a1cf6d729fbc8da9ec8c7b46d2dfd2bea184b4
Shirley Zhu <44847817+shirleymango@users.noreply.github.com>
2021-01-23 21:02:48 -0600

    Add setEvent1(), setEvent2(), update main method

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 	private static Event event1;
 	private static Event event2;
 	private static int score = 0;
+	private static int numOfLives = 3;
 	
 	public static void main(String[] args) {
 		events = new ArrayList<>(Arrays.asList(new Event("Prince Harry and Meghan Markle quit the royal family", "01/08/2020"),
@@ -16,16 +17,16 @@ public class Main {
 				new Event("\"Parasite\" sweeping the Oscars", "02/09/2020"),
 				new Event("Harvey Weinstein being sentenced to 23 years in prison", "02/24/2020"),
 				new Event("The 2020 stock market crash", "03/09/2020"),
-				new Event("The death of Breona Taylor", "03/13/2020"),
-				new Event("Kim Jon-Un death rumors", "04/15/2020"),
-				new Event("Elon Musks's private space company SpaceX launching NASA astronauts into orbit", "05/30/2020"),
-				new Event("Trump starting his trial rally in Tulsa, Oklahoma onnly to have it hijacked by K-Pop stans", "06/20/2020"),
-				new Event("Ghislaine Maxwell being arrested o multiple charges related to the sexual abuse of young women and girls by Jeffrey Epstein", "07/02/2020"),
-				new Event("Kanye West anouncing his 2020 United States presidential election campaign through Twitter", "07/04/2020"),
-				new Event("A Florida teen being arrested for taking over the Twitter accounts of Joe Biden, Bill Gates, Elon Musk, and Kanye West inn a bitcoin scam", "07/31/2020"),
-				new Event("An ammonium nitrate explosion hittig Beirut, Lebanon", "08/04/2020"),
-				new Event("Kamala Harris being chosen as Democratic VP candidate", "08/11/2020"),
-				new Event("Biden becoming the Democractic VP candidate", "08/18/2020"),
+				new Event("The death of Breonna Taylor", "03/13/2020"),
+				new Event("Kim Jong-Un death rumors", "04/15/2020"),
+				new Event("Elon Musk's private space company SpaceX launching NASA astronauts into orbit", "05/30/2020"),
+				new Event("Trump starting his trial rally in Tulsa, Oklahoma only to have it hijacked by K-Pop stans", "06/20/2020"),
+				new Event("Ghislaine Maxwell being arrested on multiple charges related to the sexual abuse of young women and girls by Jeffrey Epstein", "07/02/2020"),
+				new Event("Kanye West announcing his 2020 United States presidential election campaign through Twitter", "07/04/2020"),
+				new Event("A Florida teen being arrested for taking over the Twitter accounts of Joe Biden, Bill Gates, Elon Musk, and Kanye West in a bitcoin scam", "07/31/2020"),
+				new Event("An ammonium nitrate explosion hitting Beirut, Lebanon", "08/04/2020"),
+				new Event("Biden naming Kamala Harris as his VP running mate", "08/11/2020"),
+				new Event("Biden officially becoming the Democratic presidential nominee at the Democratic National Convention", "08/18/2020"),
 				new Event("Chadwick Boseman's death", "08/28/2020"),
 				new Event("Ruth Bader Ginsburg's death", "09/18/2020"),
 				new Event("Trump testing positive for COVID-19", "10/02/2020"),
@@ -35,20 +36,45 @@ public class Main {
 				new Event("The first Americans receiving the COVID-19 vaccine", "12/14/2020")));
 		in = new Scanner(System.in);
 		
-		event1 = events.get(0);
-		event2 = events.get(1);
-		//Printing the first pair of events
-		System.out.println("Event 1: " + event1.getName());
-		System.out.println("Date: " + event1.getDate());
-		System.out.println();
-		System.out.println("Event 2: " + event2.getName());
-		System.out.println("Date: ?");
-		System.out.println("Does Event 2 occur BEFORE or AFTER Event 1?");
-		String input = in.nextLine();
-		//checking the answer
-		checkAnswer(input);
-		//set Event 1 to be the previous Event 2
-		setEvent1();		
+		//Instantiating the first pair of events
+		int index = getRandomIndex();
+		event1 = events.get(index);
+		events.remove(index);
+		index = getRandomIndex();
+		event2 = events.get(index);
+		events.remove(index);
+		
+		while (numOfLives > 0) {
+			//Printing the current score and number of lives
+			System.out.println("Number of Lives: " + numOfLives);
+			System.out.println("Score: " + score);
+			System.out.println();
+			
+			//Printing the pair of events
+			System.out.println("Event 1: " + event1.getName());
+			System.out.println("Date: " + event1.getDate());
+			System.out.println();
+			System.out.println("Event 2: " + event2.getName());
+			System.out.println("Date: ?");
+			System.out.println("Does Event 2 occur BEFORE or AFTER Event 1?");
+			String input = in.nextLine();
+			
+			//Checking the answer
+			checkAnswer(input);
+			System.out.println("-----------------------------------------------------------------");
+			System.out.println();
+			
+			//Setting Event 1 to be the previous Event 2 and setting Event 2 to a new random event
+			setEvent1();
+			setEvent2();
+		}
+		System.out.println("Number of Lives: " + numOfLives);
+		System.out.println("Score: " + score);
+		System.out.println("Game Over.");
+	}
+	
+	public static int getRandomIndex() {
+		return (int) (Math.random() * events.size());
 	}
 	
 	public static void checkAnswer(String ans) {
@@ -61,6 +87,7 @@ public class Main {
 			System.out.println("Correct. " + event2.getName() + " happened on " + event2.getDate());
 		}
 		else {
+			numOfLives--;
 			System.out.println("Incorrect. " + event2.getName() + " happened on " + event2.getDate());
 		}
 	}
@@ -68,6 +95,12 @@ public class Main {
 	public static void setEvent1() {
 		event1.setName(event2.getName());
 		event1.setDate(event2.getDate());
-	}	
+	}
+	
+	public static void setEvent2() {
+		int index = getRandomIndex();
+		event2 = events.get(index);
+		events.remove(index);
+	}
 
 }
